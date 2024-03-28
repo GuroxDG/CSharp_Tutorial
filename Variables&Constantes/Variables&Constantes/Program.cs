a436caf290a1dc1b7fb080c8a40d92e3d48c635b
Gustavo Ibarra <gurox15@gmail.com>
2024-03-28 01:05:09 -0500

    initial commit

---
+++ Variables&Constantes/Variables&Constantes/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstName = "John";
+string lastName = "Doe";
+
+Console.WriteLine("Name: "+ firstName + " " + lastName);
+
+int numberOne = 5;
+int numberTwo = 6;
+Console.WriteLine(numberOne + numberTwo);
+
+//int numberThree = "John"; Error
+
+int numberThree;
+numberThree = 7;
+Console.WriteLine(numberOne + numberTwo + numberThree);
+
+string anyName = "Frank";
+anyName = "Gustavo";
+Console.WriteLine(anyName);
+
+
+//int a,
+//    b = 10,
+//    c = 20; Error
+
+
+int a, b, c;
+
+a = 1;
+b = 2;
+c = 3;
+
+Console.WriteLine(a + b + c);
+
+Console.WriteLine(Math.PI);
