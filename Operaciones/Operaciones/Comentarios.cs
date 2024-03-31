ca57fbd5a35c3bb7fdc2b3ce5dad6682ef95f96e
Gustavo Ibarra <gurox15@gmail.com>
2024-03-31 03:28:50 -0500

    Add Operations

---
+++ Operaciones/Operaciones/Comentarios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    /// <summary>
+    /// Comentario xml
+    /// </summary>
+    internal class Comentarios
+    {
+         
+        public void hola(string comentario)
+        {
+            Console.WriteLine(comentario);
+        }
+
+        public void inicio()
+        {
+            int x = 1; // This one liner
+            //int x = 1;
+            //this is async document
+
+            /*
+             * Comentarios multilinea
+             */
+
+            Console.WriteLine(" Hello World ");
+
+            //TODO esta es una todo class Bookmarks
+        }
+
+    }
+}
---
+++ Operaciones/Operaciones/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// arimeticos
+
+using Operaciones;
+using System.Xml.Serialization;
+
+int a = 5;
+int b = 5;
+
+Console.WriteLine(a + b);
+Console.WriteLine(a * b);
+
+int c = a + b;
+
+Console.WriteLine(c / b);
+
+Console.WriteLine( a + b * c);
+Console.WriteLine((a + b) * c);
+
+// Comparacion
+
+Console.WriteLine(b > c);
+Console.WriteLine(b == c);
+Console.WriteLine(b < c);
+Console.WriteLine(b < c && a > c);
+Console.WriteLine(b < c || a > c);
+Console.WriteLine(!(b < c || a > c));
+Console.WriteLine(b != c);
+
+Comentarios comentarios = new Comentarios();
+comentarios.hola("Hellow World");
